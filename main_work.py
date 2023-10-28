66a05ac6e00a2c6833ab24ae905af1d82e15a1a3
Alex <sasha.blinov.04@bk.ru>
2023-10-28 12:22:03 +0200

    first_file

---
+++ main_work.py
@@ -0,0 +1,13 @@
+chat_story = ""
+command = input("Введите любой символ для начала работы чата (чтобы остановить работу чата, напишите 'end'): ")
+while command != "end":
+    user_name = input("\nВведите имя пользователя: ")
+    command = input("\n1 - Посмотреть текст чата\n2 - Отправить сообщение\n3 - Остановить чат\nВыберете команду: ")
+    if command == "1":
+        print("\nИстория чата: ", chat_story)
+    if command == "2":
+        message = input("Введите сообщение: ")
+        chat_story += user_name + ": " + message + "\n"
+    if command == "3":
+        print("Завершение работы чата.")
+        break
